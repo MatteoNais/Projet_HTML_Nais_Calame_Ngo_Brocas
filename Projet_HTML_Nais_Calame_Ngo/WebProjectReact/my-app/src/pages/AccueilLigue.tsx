8e5075f87d8161012d9ced2d31cc132f7db4af7f
Matteo <naismatteo72@gmail.com>
2024-02-21 13:41:51 +0100

    ajout choix de la date de la première draft.

--- Projet_HTML_Nais_Calame_Ngo/WebProjectReact/my-app/src/pages/AccueilLigue.tsx
+++ Projet_HTML_Nais_Calame_Ngo/WebProjectReact/my-app/src/pages/AccueilLigue.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Box, Button, Grid, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Grid, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Classement from '../components/Classement';
 import './StylePages.css';
+import TextField from '@mui/material/TextField';
+import dayjs, { Dayjs } from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
 function AccueilLigue() {
+    const [value, setValue] = React.useState<Dayjs | null>(dayjs());
+    const options = ['Bleu', 'Rouge', 'Vert', 'Jaune', 'Orange'];
+
+    const theme = createTheme({
+        palette: {
+            primary: { main: "#E36414" }
+        }
+    });
 
     return (
         <div className="body">
             <div className='App-body-ligue'>
-                <Grid container spacing={2} sx={{ marginTop: '2vh' }}>
+                <Grid container spacing={3} sx={{ marginTop: '2vh' }}>
                     <Grid item xs={6}>
                         {/* Colonne de gauche */}
                         <Grid item xs={12}>
@@ -20,8 +35,27 @@ function AccueilLigue() {
                     <Grid item xs={6}>
                         {/* Classement des joueurs avec recap des points */}
                         <Classement></Classement>
-                        <Box style={{ textAlign: 'center' }}>
-                            <Button variant="contained" color="warning" style={{ width: 'fit-content' }}>
+                        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'row', marginLeft: '10px', marginRight: '10px' }}>
+                            <ThemeProvider theme={theme}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fr" >
+                                    <DateTimePicker
+                                        label="Date de la première draft"
+                                        value={value}
+                                        onChange={(newValue: Dayjs | null) => setValue(newValue)} selectedSections={undefined} onSelectedSectionsChange={undefined}
+                                        sx={{
+                                            "input": { color: "white" }, "fieldSet": { borderColor: "white" }, "&:hover .MuiOutlinedInput-notchedOutline": {
+                                                borderColor: "white", // Change this to your desired color on hover
+                                            }, "label": { color: "white" }, "button *": { color: "warning.main" }
+                                        }}
+                                        slotProps={{
+                                            textField: {
+                                                color: "warning"
+                                            }
+                                        }} />
+
+                                </LocalizationProvider>
+                            </ThemeProvider>
+                            <Button variant="contained" color="warning" style={{ width: 'fit-content', marginLeft: '20px' }}>
                                 <Typography variant="h5" sx={{ color: 'white' }}>Démarrer la draft</Typography>
                             </Button>
                         </Box>
--- Projet_HTML_Nais_Calame_Ngo/WebProjectReact/my-app/src/pages/DraftLigue.tsx
+++ Projet_HTML_Nais_Calame_Ngo/WebProjectReact/my-app/src/pages/DraftLigue.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './StylePages.css';
-import { Avatar, Button, Grid, Link, List, ListItem, ListItemAvatar, ListItemText, Typography} from "@mui/material";
+import { Avatar, Button, Grid, Link, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import axiosInstance from "../api/axiosInstance";
 import teamNBA from "../objects/TeamNBA";
 import playerNBA from "../objects/playerNBA";
@@ -31,14 +31,14 @@ function DraftLigue() {
     console.log(basicUserInfo?.id);
     useEffect(() => {
         axiosInstance.get(`/equipe/byLigueAndUser/${ligueId}/${basicUserInfo?.id}`) // Remplacez par l'URL de votre API
-                .then(response => {
-                    console.log(response.data);
-                    setThisTeam(response.data.equipe.id);
-                    console.log(response.data.equipe);
-                    console.log(response.data.equipe.id);
-                    console.log(this_team);
-                })
-                .catch(error => console.error('Error:', error));
+            .then(response => {
+                console.log(response.data);
+                setThisTeam(response.data.equipe.id);
+                console.log(response.data.equipe);
+                console.log(response.data.equipe.id);
+                console.log(this_team);
+            })
+            .catch(error => console.error('Error:', error));
     }, [ligueId]);
 
 
@@ -51,29 +51,29 @@ function DraftLigue() {
             .catch(error => console.error('Error:', error));
     }, [ligueId, basicUserInfo?.id]);
 
-    
+
 
     useEffect(() => {
         axiosInstance.get(`/equipe/byLigue/${ligueId}`) // Remplacez par l'URL de votre API
-                .then(response => {
-                    console.log(response.data);
-                    setUsers(response.data.users);
-                })
-                .catch(error => console.error('Error:', error));
+            .then(response => {
+                console.log(response.data);
+                setUsers(response.data.users);
+            })
+            .catch(error => console.error('Error:', error));
     }, [ligueId]);
 
-    
+
 
     useEffect(() => {
         axiosInstance.get(`/teamsNBA/all`) // Remplacez par l'URL de votre API
-                .then(response => {
-                    console.log(response.data);
-                    setTeams(response.data.team);
-                })
-                .catch(error => console.error('Error:', error));
+            .then(response => {
+                console.log(response.data);
+                setTeams(response.data.team);
+            })
+            .catch(error => console.error('Error:', error));
     }, []);
-    
-    async function getPlayerfromteam(equipe_id:number) {
+
+    async function getPlayerfromteam(equipe_id: number) {
         axiosInstance.get(`/playersNBA/team/${equipe_id}`) // Remplacez par l'URL de votre API
             .then(response => {
                 console.log(response.data);
@@ -82,25 +82,25 @@ function DraftLigue() {
             .catch(error => console.error('Error:', error));
     }
 
-    async function getPlayerCard(player_id:number) {
+    async function getPlayerCard(player_id: number) {
         axiosInstance.get(`/playersNBA/${player_id}`) // Remplacez par l'URL de votre API
-          .then(response => {
+            .then(response => {
                 console.log(response.data);
                 setSelectedPlayer(response.data.player);
                 /*id_joueur = response.data.player.id;
                 console.log(id_joueur);*/
             })
-          .catch(error => console.error('Error:', error));
+            .catch(error => console.error('Error:', error));
     }
 
-    async function draftPlayer(team_id:number, player_id:number) {
+    async function draftPlayer(team_id: number, player_id: number) {
         try {
             console.log(team_id, player_id);
             const response = await axiosInstance.post(`/equipe/addJoueurNBA/${team_id}/${player_id}`, {
                 // team_id: team_id,
                 // player_id: player_id
             });
-    
+
             console.log(response.data);
             setSelectedPlayer(response.data.player);
         } catch (error) {
@@ -108,104 +108,105 @@ function DraftLigue() {
         }
     }
 
-    const teamItemClick = (key:number) => {
+    const teamItemClick = (key: number) => {
         getPlayerfromteam(key);
         setSelectedTeamItem(key);
     };
 
-    const joueurItemClick = (key:number) => {
+    const joueurItemClick = (key: number) => {
         getPlayerCard(key);
         setSelectedPlayerItem(key);
         //console.log("action a effectuer en fonction du joueur "+ key)
     };
 
-    const draftPlayerClick = (teamkey:number, playerkey:number) => {
+    const draftPlayerClick = (teamkey: number, playerkey: number) => {
         draftPlayer(teamkey, playerkey);
     };
 
     return (
         <div className="body">
-        <div className='App-body-ligue'>
-        <Grid container spacing={2} sx={{ marginTop: '5vh', textAlign: 'center'  }}>
-                <Grid item xs={12}>
-                    {/* Ligne avec le type de draft et la recherche avancée et compteur */}
-                </Grid>
-                <Grid item xs={4}>
+            <div className='App-body-ligue'>
+                <Grid container spacing={2} sx={{ marginTop: '5vh', textAlign: 'center' }}>
                     <Grid item xs={12}>
-                        {/* Info n tour */}
+                        {/* Ligne avec le type de draft et la recherche avancée et compteur */}
                     </Grid>
-                    <Grid item xs={12} bgcolor={"white"} color={"black"}>
-                        <Typography variant="h6">
-                            Récapitulatif de votre draft
-                        </Typography>
-                        <List>
+                    <Grid item xs={4} sx={{ marginLeft: 4, marginRight: 4 }}>
+                        <Grid item xs={12}>
+                            {/* Info n tour */}
+                        </Grid>
+
+                        <Grid item xs={12} bgcolor={"white"} color={"white"} sx={{ height: '50vh', display: 'flex', flexWrap: 'wrap', backgroundColor: "grey", border: "3px solid #ff6a00" }}>
+                            <Grid item xs={12} md={12}>
+                                <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+                                    Récapitulatif de votre draft
+                                </Typography>
+                            </Grid>
                             {/* Vérifiez d'abord si joueurs est défini pour éviter les erreurs */}
                             {joueurs && joueurs.map(joueur => (
-                                <ListItem key={joueur.id}>
+                                <Grid key={joueur.id} item xs={12} sm={6} md={4} lg={3} >
                                     <img
                                         src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${joueur.id}.png`}
                                         alt="Image joueur"
-                                        style={{ width: "15%", height: "auto" }}
+                                        style={{ width: "80%", height: "auto" }}
                                     />
-                                    <ListItemText primary={joueur.prenom + " " + joueur.nom} />
-                                </ListItem>
+                                    <Typography sx={{ fontWeight: 'bold' }}> {joueur.prenom + " " + joueur.nom} </Typography>
+                                </Grid>
                             ))}
-                        </List>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid item xs={2} className="liste_equipeNBA">
-                    <Typography variant="h6">
+                    <Grid item xs={2} className="liste_equipeNBA">
+                        <Typography variant="h6">
                             Equipe
                         </Typography>
                         <List className="list">
                             {teams.map(team => (
-                                <ListItem    key={team.id} 
-                                            className="listItem" 
-                                            style={{ backgroundColor: selectedTeamItem === team.id ? 'lightgrey' : 'grey' }} 
-                                            onClick={() => teamItemClick(team.id)}>
-                                <ListItemAvatar>
-                                    <Avatar alt={`${team.nom }`} src={` https://cdn.nba.com/logos/nba/${team.id}/global/L/logo.svg `} />
-                                </ListItemAvatar>
-                                <ListItemText primary={`${team.nom  }`} />
+                                <ListItem key={team.id}
+                                    className="listItem"
+                                    style={{ backgroundColor: selectedTeamItem === team.id ? 'lightgrey' : 'grey' }}
+                                    onClick={() => teamItemClick(team.id)}>
+                                    <ListItemAvatar>
+                                        <Avatar alt={`${team.nom}`} src={` https://cdn.nba.com/logos/nba/${team.id}/global/L/logo.svg `} />
+                                    </ListItemAvatar>
+                                    <ListItemText primary={`${team.nom}`} />
                                 </ListItem>
                             ))}                    {/* Selection des equipes */}
                         </List>
-                </Grid>
-                <Grid item xs={2} className="liste_joueurNBA">
+                    </Grid>
+                    <Grid item xs={2} className="liste_joueurNBA">
                         <Typography variant="h6">
                             Joueurs
                         </Typography>
                         <List className="list">
                             {players.map(player => (
-                                <ListItem   key={player.id} 
-                                            className="listItem" 
-                                            style={{ backgroundColor: selectedPlayerItem === player.id ? 'lightgrey' : 'grey' }}
-                                            onClick={() => joueurItemClick(player.id)}>
-                                <ListItemAvatar>
-                                    <Avatar alt={`${player.prenom + " " + player.nom  }`} src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${player.id}.png`} />
-                                </ListItemAvatar>
-                                <ListItemText primary={`${player.prenom + " " + player.nom  }`} />
-                            </ListItem>
+                                <ListItem key={player.id}
+                                    className="listItem"
+                                    style={{ backgroundColor: selectedPlayerItem === player.id ? 'lightgrey' : 'grey' }}
+                                    onClick={() => joueurItemClick(player.id)}>
+                                    <ListItemAvatar>
+                                        <Avatar alt={`${player.prenom + " " + player.nom}`} src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${player.id}.png`} />
+                                    </ListItemAvatar>
+                                    <ListItemText primary={`${player.prenom + " " + player.nom}`} />
+                                </ListItem>
                             ))}                    {/* Liste des joueurs */}
                         </List>
-                </Grid>
-                <Grid item xs={2}>
+                    </Grid>
+                    <Grid item xs={2}>
                         <Typography variant="h6">
                             Carte joueur :
                         </Typography>
                         {selected_player && (
-                        <>
-                            <CarteJoueur key={selected_player.id} joueurId={selected_player.id as number} onScoreChange={(score) => console.log(score)} />
-                            <p> Id joueur : {selected_player.id}</p>
-                            <Button variant="contained" disableElevation onClick={() => draftPlayerClick(this_team as number, selected_player?.id as number)}>
-                                <b>Drafter le joueur</b>
-                            </Button>
-                        </>
-                    )}
+                            <>
+                                <CarteJoueur key={selected_player.id} joueurId={selected_player.id as number} onScoreChange={(score) => console.log(score)} />
+                                <p> Id joueur : {selected_player.id}</p>
+                                <Button variant="contained" disableElevation onClick={() => draftPlayerClick(this_team as number, selected_player?.id as number)}>
+                                    <b>Drafter le joueur</b>
+                                </Button>
+                            </>
+                        )}
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div>
         </div>
-    </div>
     );
 }
 
